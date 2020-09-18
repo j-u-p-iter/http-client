b0bd101027a7d7d8e6f5f1cc706a8a37a6d8d334
j.u.p.iter <pn79122821224@gmail.com>
2020-09-18 05:52:48 +0500

    feat(HttpClient): creates HttpClient

--- src/HttpClient.ts
+++ src/HttpClient.ts
@@ -1,3 +1,16 @@
+/**
+ * HTTP service - wrapper around axios.
+ *
+ * The goal of this service:
+ *   - make any http process easy to cancel;
+ *   - simplify uploading files operation.
+ *
+ * To make cancel operation possible we create CustomAxiosPromise.
+ * The goal of CustomAxiosPromise object is to extend AxiosPromise interface with additional "cancel" method.
+ *
+ */
+import { AxiosInstance, AxiosPromise } from "axios";
+
 interface HttpClientInterface {
   read: (path: string) => Promise<any>;
   create: (path: string, data: any) => Promise<any>;
@@ -6,38 +19,61 @@ interface HttpClientInterface {
   upload: (path: string, data: any) => Promise<any>;
 }
 
-class HttpClient implements HttpClientInterface {
-  public read() {
+const makeCancellable = <T>(
+  request: AxiosPromise<T>
+): CustomAxiosPromise<T> => {
+  const cancellableRequest = {
+    ...request,
+    cancel: () => {}
+  };
 
+  return cancellableRequest;
+};
+
+interface CustomAxiosPromise<T> extends AxiosPromise<T> {
+  cancel: () => void;
+}
+
+export class HttpClient implements HttpClientInterface {
+  constructor(private axios: AxiosInstance) {}
+
+  public read<T>(path: string): CustomAxiosPromise<T> {
+    const request = this.axios.get<T>(path);
+
+    return makeCancellable<T>(request);
   }
 
-  public create() {
+  public create<T>(path: string, data: any): CustomAxiosPromise<T> {
+    const request = this.axios.post<T>(path, data);
 
+    return makeCancellable<T>(request);
   }
 
-  public update() {
+  public update<T>(path: string, data: any): CustomAxiosPromise<T> {
+    const request = this.axios.put<T>(path, data);
 
+    return makeCancellable<T>(request);
   }
 
-  public delete() {
+  public delete<T>(path: string): CustomAxiosPromise<T> {
+    const request = this.axios.put<T>(path);
 
+    return makeCancellable<T>(request);
   }
 
-  public upload(path, files) {
+  public upload<T>(path: string, files: File[]): CustomAxiosPromise<T> {
     const formData = new FormData();
 
     files.forEach((file, index) => {
-      formData.append(`files[${index}]`, file)
-    }); 
+      formData.append(`files[${index}]`, file);
+    });
 
-    return axios.post(
-      path,
-      formData,
-      {
-        headers: {
-          'Content-Type': 'multipart/form-data',
-        }
+    const request = this.axios.post<T>(path, formData, {
+      headers: {
+        "Content-Type": "multipart/form-data"
       }
-    );
+    });
+
+    return makeCancellable<T>(request);
   }
 }
